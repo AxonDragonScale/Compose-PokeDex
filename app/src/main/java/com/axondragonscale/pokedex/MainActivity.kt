a954faa62f508caefbc7517ca3d939b136686e7f
Ronak Harkhani <ronak.harkhani@phonepe.com>
2021-05-25 00:33:38 +0530

    Added Gradle dependencies and plugins.

--- app/src/main/java/com/axondragonscale/pokedex/MainActivity.kt
+++ app/src/main/java/com/axondragonscale/pokedex/MainActivity.kt
@@ -7,32 +7,41 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavArgument
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
 import com.axondragonscale.pokedex.ui.theme.PokeDexTheme
 
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        const val PokemonListScreen = "PokemonListScreen"
+
+        const val PokemonDetailScreen = "PokemonDetailScreen"
+        const val DominantColorArg = "DominantColorArg"
+        const val PokemonNameArg = "PokemonNameArg"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PokeDexTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     PokeDexTheme {
-        Greeting("Android")
+
     }
 }
